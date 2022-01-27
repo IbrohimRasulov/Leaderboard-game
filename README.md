0ba497005cbe7db03d79532d9ff1b62992a02602
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-27 19:55:42 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Webpack-template
+
+> This is a ready webpack template that can be used when you start a new project.
+
+### Prerequisites
+
+    - node
+
+### Setup
+
+> Clone the repository to your local machine and run:
+```
+npm install
+npm install --save-dev webpack
+npm install --save lodash
+npx webpack --config webpack.config.js
+npm install webpack webpack-cli --save-dev
+npm install --save-dev style-loader css-loader
+npm install --save-dev csv-loader xml-loader
+npm install --save-dev html-webpack-plugin
+npm install --save-dev webpack-dev-server
+```
+
+### Deployment
+
+ > run:
+ ```
+ npm run build
+ npm start
+ ```
+
+## Authors
+
+👤 **Ibrohim Rasulov**
+
+- [GitHub](https://github.com/IbrohimRasulov)
+- [Twitter](https://twitter.com/IbrohimRasu1ov)
+- [LinkedIn](https://www.linkedin.com/in/ibrohim-rasulov-a88352209/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
